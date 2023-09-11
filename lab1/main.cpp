5130f5df000d31a0fb4cc0d388c602473091fda4
v.rabetskii <v.rabetskii@g.nsu.ru>
2023-09-11 23:20:02 +0700

    main file

---
+++ lab1/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+
+    long long n = 100000000;
+    double pi = 0;
+    double temp;
+    timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+
+
+    for (int i = 0; i < n; i++)
+    {
+        temp = ((pow(-1, i)) / (2 * i + 1));
+        pi += temp;
+    }
+    pi = 4 * pi;
+
+    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+
+    cout << "start = " << start.tv_sec << endl;
+    cout << "end = "<< end.tv_sec << endl;
+    cout << "pi = " << pi << endl;
+    return 0;
+
+}
